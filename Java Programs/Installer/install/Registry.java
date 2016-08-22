ac3fd89903f2c61f9c7627b81aae1f8c76b7423e
Tabor Kvasnicka <kamakwazee@gmail.com>
2016-08-21 20:04:44 -0500

    Added XML for Registry
    I added XML capabilities for the version in registry to allow for
    updatability.

--- Java Programs/Installer/install/Registry.java
+++ Java Programs/Installer/install/Registry.java
@@ -1,11 +1,21 @@
 package install;
 
-import java.time.LocalDateTime;
-
 import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.WinReg;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.time.LocalDateTime;
 
 public class Registry {
+
+	private int vmaj, vmin;
+	private String version;
 	
 	private String getDateString(int dateValue)
 	{
@@ -16,6 +26,40 @@ public class Registry {
 		return Integer.toString(dateValue);
 		
 	}
+
+	private void readXML()
+	{
+
+		try
+		{
+
+			File fXmlFile = new File("info.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			doc.getDocumentElement().normalize();
+
+			NodeList nList = doc.getElementsByTagName("chalkboard");
+
+			Node nNode = nList.item(0);
+
+			if(nNode.getNodeType() == Node.ELEMENT_NODE)
+			{
+
+				Element eElement = (Element) nNode;
+				vmaj = Integer.parseInt(eElement.getAttribute("major"));
+				vmin = Integer.parseInt(eElement.getAttribute("minor"));
+				version = eElement.getAttribute("version");
+
+			}
+
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
 	
 	public void write(String GUID, String installPath, int estimatedSize)
 	{
@@ -24,10 +68,10 @@ public class Registry {
 		
 		Advapi32Util.registryCreateKey(WinReg.HKEY_LOCAL_MACHINE, regPath);
 		Advapi32Util.registrySetStringValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "DisplayName", "Chalkboard");
-		Advapi32Util.registrySetStringValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "DisplayVersion", "3.0");
+		Advapi32Util.registrySetStringValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "DisplayVersion", version);
 		Advapi32Util.registrySetStringValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "Publisher", "Kamakwazee Open Source Team");
-		Advapi32Util.registrySetIntValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "VersionMajor", 3);
-		Advapi32Util.registrySetIntValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "VersionMinor", 0);
+		Advapi32Util.registrySetIntValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "VersionMajor", vmaj);
+		Advapi32Util.registrySetIntValue(WinReg.HKEY_LOCAL_MACHINE, regPath, "VersionMinor", vmin);
 		LocalDateTime date = LocalDateTime.now();
 		String dateString = Integer.toString(date.getYear()) + getDateString(date.getMonthValue()) + getDateString(date.getDayOfMonth());
 		System.out.println(dateString);
